b2f7d0e66b8a2c76f6a54243e81cc35351d45bdf
Abhiramabvr <2338018077@webmail.uad.ac.id>
2026-06-02 19:52:51 +0700

    Menambahkan routing Javascript dan fitur scroll navbar

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,10 @@
 import axios from 'axios';
-window.axios = axios;
+import { initScrollSpy } from './nav-scroll'; // Pastikan path-nya benar
 
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// Jalankan fungsinya
+document.addEventListener('DOMContentLoaded', () => {
+    initScrollSpy();
+});
