cfdc3f548b256c3da62faf41a5a99f4f2bc68b94
Abhiramabvr <2338018077@webmail.uad.ac.id>
2026-06-02 19:54:21 +0700

    Edit edit detailing routing dan order table

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LayananController;
 
 Route::get('/', function () {
     return view('pages.home');
 });
+
+Route::get('/layanan/{slug}', [LayananController::class, 'show'])->name('layanan.show');
+Route::post('/layanan/{slug}/order', [LayananController::class, 'store'])->name('layanan.store');
+
